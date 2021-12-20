9e634432fcf6c6d27b13d1d249427abb06a409fd
ApplePie <novikov1089@gmail.com>
2021-12-20 16:59:22 +0500

    fixed bag with second start EXE

--- Core.py
+++ Core.py
@@ -1,11 +1,13 @@
 import os
 import shutil
 
+
 def change_name():
     result = os.listdir()
     i = 0
     for temp in result:
-        if (temp.count('-') >= 4) and ('.pdf' in temp) and ('ОЛ' not in temp) and ('ИСХ' not in temp) and ('.С' not in temp):
+        if ((temp.count('-') > 4) or (('.Н' in temp) and (temp.count('-') != 3))) and ('.pdf' in temp) and (
+                'ОЛ' not in temp) and ('ИСХ' not in temp) and ('.С' not in temp):
             test_set = temp.split('-')
             test_set.pop()
             if len(test_set) >= 6:
@@ -23,4 +25,3 @@ def change_name():
 
 if __name__ == '__main__':
     change_name()
-
